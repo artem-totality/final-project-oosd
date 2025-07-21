647ee76d86683ca893fe9abab0c672887e9488ea
artem-totality <artem.totality@gmail.com>
2025-07-21 19:57:10 +0100

    Add part of comments to project

--- ie/atu/sw/Engine.java
+++ ie/atu/sw/Engine.java
@@ -1,63 +1,77 @@
 package ie.atu.sw;
 
-import java.io.File;
 import java.util.Arrays;
 import java.util.Scanner;
 
+// record Item has two properties base and code of type String
+// base stores text part of the mapping file
+// code stores text value of the corresponding code
+// also Item implements interface Comparable
+// it needs for sorting words and suffixes in order to optimisation process of encoding
 record Item(String base, String code) implements Comparable<Item> {
     public int compareTo(Item i) {
+        // firstly we sort words by length
         if (this.base().length() != i.base().length()) {
             return i.base().length() - this.base().length();
         }
 
+        // and then by alphabet
         return this.base().compareTo(i.base());
     }
 }
 
 public class Engine {
+    // predefine SEPARATOR that we use during encoding process to separate code
+    // values
     private static final String SEPARATOR = ", ";
+    // variable for storing mapping file name with predefined default value
     private static String mappingFileName = "./encodings-10000.csv";
-    private static String workFileName = "./lord.txt";
+    // variable for storing work file name with predefined default value
+    private static String workFileName = "./test.txt";
+    // variable for storing output file name with predefined default value
     private static String outputFileName = "./out.txt";
-    private static Item[] words;
-    private static Item[] suffixes;
+    // work document stored as array of String
     private static String[] workDocument;
+    // array for storing mapped words with codes is used during encoding
+    private static Item[] words;
+    // array for storing mapped suffixes with codes is used during encoding
+    private static Item[] suffixes;
+    // array of mapped words uploaded as plain String array where every
+    // code of the word or suffix is equal to its index
+    // this array is used during decoding
     private static String[] plainVocabulary;
 
-    public static boolean validateStringFilenameUsingIO(String filename) {
-        File file = new File(filename);
-        boolean created = false;
-        try {
-            created = file.createNewFile();
-            return created;
-        } catch (Exception e) {
-            return created;
-        } finally {
-            if (created) {
-                file.delete();
-            }
-        }
-    }
+    // Method transforms all lines of the work document to lower case
+    // cause mapping file consists of only low case words and suffixes
+    private static String[] transformToLowerCase(String[] lines) {
+        var result = new String[lines.length];
 
-    private static String[] transformToLowCase(String lines[]) {
         for (var i = 0; i < lines.length; i++) {
-            lines[i] = lines[i].toLowerCase();
+            result[i] = lines[i].toLowerCase();
         }
 
-        return lines;
+        return result;
     }
 
-    private static void parseVocabulary(String lines[]) {
+    // Method parses mapping words
+    // separate words and suffixes and sotrs them
+    private static void parseVocabulary(String lines[]) throws Exception {
+        // reset words and suffixes counters
         var suffixesCounter = 0;
         var wordsCounter = 0;
 
+        // firstly define count of words and suffixes
         for (var line : lines) {
+            // split line by coma
             var parts = line.split(",");
 
+            // if a line consists of more or less than two parts (base, code)
+            // then skip this line
             if (parts.length != 2) {
                 continue;
             }
 
+            // count separately words and suffixes
             if (parts[0].startsWith("@@")) {
                 suffixesCounter += 1;
             } else {
@@ -65,118 +79,176 @@ public class Engine {
             }
         }
 
+        // initialize arrays with defined count of words and suffixes
         Engine.words = new Item[wordsCounter];
         Engine.suffixes = new Item[suffixesCounter];
+        // initialize plain vocabulary with size words plus suffixes
         Engine.plainVocabulary = new String[wordsCounter + suffixesCounter];
+        // reset words and suffixes counters
         wordsCounter = 0;
         suffixesCounter = 0;
 
-        for (var line : lines) {
-            var parts = line.split(",");
+        try {
+            // for every line in array
+            for (var line : lines) {
+                // split line by coma
+                var parts = line.split(",");
 
-            if (parts.length != 2) {
-                continue;
+                // if a line consists of more or less than two parts (base, code)
+                // then skip this line
+                if (parts.length != 2) {
+                    continue;
+                }
+
+                // save current word or suffix with corresponding index to the plain voacabulary
+                Engine.plainVocabulary[wordsCounter + suffixesCounter] = parts[0];
+
+                // check is it word or suffix and then add it to appropriate array
+                // also increase corresponding counter
+                if (parts[0].startsWith("@@")) {
+                    Engine.suffixes[suffixesCounter] = new Item(parts[0].substring(2), parts[1]);
+                    suffixesCounter += 1;
+                } else {
+                    Engine.words[wordsCounter] = new Item(parts[0], parts[1]);
+                    wordsCounter += 1;
+                }
             }
 
-            Engine.plainVocabulary[wordsCounter + suffixesCounter] = parts[0];
-
-            if (parts[0].startsWith("@@")) {
-                Engine.suffixes[suffixesCounter] = new Item(parts[0].substring(2), parts[1]);
-                suffixesCounter += 1;
-            } else {
-                Engine.words[wordsCounter] = new Item(parts[0], parts[1]);
-                wordsCounter += 1;
-            }
+            // in case of error throw exception with appropriate message
+        } catch (Exception e) {
+            throw new Exception("Vocabulary parsing ERROR!!!");
         }
 
+        // After all words and suffixes uploaded sort them for further work
         Arrays.sort(Engine.words, (a, b) -> a.compareTo(b));
         Arrays.sort(Engine.suffixes, (a, b) -> a.compareTo(b));
     }
 
+    // method reads and parses vocabulary from mapping file
     public static void readVocabulary() throws Exception {
+        // read file as String array
         var lines = FileIO.readFile(Engine.mappingFileName);
 
+        // check if file is not emty
         if (lines.length == 0) {
             throw new Exception("Empty vocabulary!!!");
         }
 
+        // tries to parse uploaded file
         Engine.parseVocabulary(lines);
     }
 
+    // method sets mapping file name and invokes readVocabulary method
     public static void uploadVocabulary() {
-        // Using Scanner for Getting Input from User
+        // using scanner for getting input from user
         System.out.print("Input Mapping File Name [Default - " + Engine.mappingFileName + "]> ");
         Scanner s = new Scanner(System.in);
+        // read user input to the fileName variable
         var fileName = s.nextLine();
 
+        // if fileName variable is not emty - set new mapping file name
+        // else skip and use default (predefined) value
         if (fileName.length() != 0) {
             Engine.mappingFileName = fileName;
         }
 
+        // print out current mapping file name
         System.out.println("Current mapping file: " + Engine.mappingFileName);
 
+        // try to upload vocabulary from the mapping file
         try {
             Engine.readVocabulary();
             System.out.println("Was Uploaded:");
             System.out.println("Words - " + Engine.words.length);
             System.out.println("Words Suffixes - " + Engine.suffixes.length);
+
+            // in case of exception - print error message
+            // and reset all words, suffixes and plainVocabulary to null
         } catch (Exception e) {
             System.err.println(e.getMessage());
             Engine.words = null;
             Engine.suffixes = null;
+            Engine.plainVocabulary = null;
         }
     }
 
+    // method reads work file and saves it to the workDocument variable
+    // as String array
     public static void readWorkFile() throws Exception {
+        // read work file as String array
         var lines = FileIO.readFile(Engine.workFileName);
-        lines = Engine.transformToLowCase(lines);
 
+        // check if file is empty then throw exception
         if (lines.length == 0) {
             throw new Exception("Empty work file!!!");
         }
 
+        // transform all uploaded lines to lower case
+        lines = Engine.transformToLowerCase(lines);
+
+        // save all lines to workDocument variable for further performing
         Engine.workDocument = lines;
     }
 
+    // method sets work file name and invokes readWorkFile method
     public static void uploadWorkFile() {
+        // using scanner for getting input from user
         System.out.print("Input Work File Name [Default - " + Engine.workFileName + "]> ");
         Scanner s = new Scanner(System.in);
+        // read user input to the fileName variable
         var fileName = s.nextLine();
 
+        // if fileName variable is not emty - set new work file name
+        // else skip and use default (predefined) value
         if (fileName.length() != 0) {
             Engine.workFileName = fileName;
         }
 
+        // print out current work file name
         System.out.println("Current work file: " + Engine.workFileName);
 
+        // try to upload work document from the specified file
         try {
             Engine.readWorkFile();
-
             System.out.println("Lines uploaded: " + Engine.workDocument.length);
+
+            // in case of exception - print error message
+            // and reset work document to null
         } catch (Exception e) {
             System.err.println(e.getMessage());
             Engine.workDocument = null;
         }
     }
 
+    // method sets ouitput file name
+    // and validates it
     public static void specifyOutputFile() {
+        // using scanner for getting input from user
         System.out.print("Specify Output File Name [Default - " + Engine.outputFileName + "]> ");
         Scanner s = new Scanner(System.in);
+        // read user input to the fileName variable
         var fileName = s.nextLine();
 
+        // validate output file name
+        // (check is it possible to use such file name for output)
         if (fileName.length() != 0) {
-            if (Engine.validateStringFilenameUsingIO(fileName)) {
+            if (FileIO.validateStringFilenameUsingIO(fileName)) {
                 Engine.outputFileName = fileName;
             } else {
+                // if entered file name is invalid show appropriate message
                 System.out.println("Invalid file name!!!");
             }
         }
 
+        // print out current output file name
         System.out.println("Current output file: " + Engine.outputFileName);
     }
 
+    // show current system status
     public static void showSystemStatus() {
+        // print out current mappping file name
         System.out.println("Current mapping file: " + Engine.mappingFileName);
+        // print out whether uploaded or not words and suffixes and statistic
         if (Engine.words != null && Engine.suffixes != null) {
             System.out.println("Was Uploaded:");
             System.out.println("Words - " + Engine.words.length);
@@ -184,52 +256,69 @@ public class Engine {
         } else {
             System.out.println("Mapping file is not uploaded.");
         }
-        System.out.println();
 
+        // print out current output file name
+        System.out.println();
         System.out.println("Current work file: " + Engine.workFileName);
+        // print out whether uploaded or not work document
         if (Engine.workDocument != null) {
             System.out.println("Lines uploaded: " + Engine.workDocument.length);
         } else {
             System.out.println("Work file is not uploaded.");
         }
-        System.out.println();
 
+        // print out current output file name
+        System.out.println();
         System.out.println("Current output file: " + Engine.outputFileName);
     }
 
+    // method decodes one line from codes to text
     private static String decodeLine(String line) throws Exception {
+        // initialise resulting string
         var decodedLine = "";
+        // split line into codes using predefined SEPARATOR
         var codes = line.split(Engine.SEPARATOR);
 
+        // try to restore string using obtained codes
         try {
-            for (var i = 0; i < codes.length; i++) {
-                var code = codes[i];
-
+            // for every code in codes
+            for (var code : codes) {
+                // skip empty lynes
                 if (code.length() == 0)
                     continue;
 
+                // parse code as integer number
                 var codeInt = Integer.parseInt(code);
+                // get corresponding word or suffix from plainVocabulary using integer index
                 var textItem = Engine.plainVocabulary[codeInt];
 
+                // check wether it is word or suffix and concatinate it
+                // to resulting string with corresponding spaces
                 if (textItem.startsWith("@@")) {
                     decodedLine = decodedLine.concat(textItem.substring(2));
                 } else {
-                    decodedLine = decodedLine.concat(i == 0 ? textItem : " ".concat(textItem));
+                    decodedLine = decodedLine.concat(decodedLine.isEmpty() ? textItem : " ".concat(textItem));
                 }
             }
+            // in case of error throw new exception with appropriate message
         } catch (Exception e) {
+            System.out.println();
             throw new Exception("Decoding error!!!");
         }
 
+        // return result
         return decodedLine;
     }
 
+    // method decodes uploaded work document
     public static void decodeTextFile() {
+        // check if vocabulary is uploaded
         if (Engine.plainVocabulary == null) {
             System.out.println("Mapping file is not uploaded.");
             return;
         }
 
+        // check if work document is uploaded
         if (Engine.workDocument == null) {
             System.out.println("Work file is not uploaded.");
             return;
@@ -240,34 +329,52 @@ public class Engine {
         System.out.println();
 
         try {
+            // try to decode uploaded work document line by line
+            // also print current progress
             for (var i = 0; i < workDocument.length; i++) {
                 outputDocument[i] = Engine.decodeLine(workDocument[i]);
                 ConsoleIO.printProgress(i + 1, workDocument.length);
             }
 
+            // in case of success save decoded document to output file
             FileIO.writeFile(Engine.outputFileName, outputDocument);
+
+            // in case of error print corresponding error message
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
     }
 
+    // method encodes given token
     private static String[] encodeToken(String token) {
+        // if a token equals to space then just skip it
         if (token.equals(" "))
             return new String[0];
 
+        // then try to search appropirate word and suffix
         for (var word : Engine.words) {
+            // if token start with given word
             if (token.startsWith(word.base())) {
+                // if token and word length are equal then - match
+                // return corresponding code
                 if (token.length() == word.base().length()) {
                     return new String[] { word.code() };
                 }
 
+                // if token longer than word - obtain reminder
                 var wordReminder = token.substring(word.base().length());
 
+                // try to pick up appropriate suffix
                 for (var suffix : suffixes) {
+                    // if on certain iteration suffix become shorter than reminder
+                    // then break from the loop cause suffixes sorted from longer to shorter
+                    // and there is no point in continuing the search
                     if (wordReminder.length() > suffix.base().length()) {
                         break;
                     }
 
+                    // if we found exact suffix then return both codes
+                    // [word, suffix]
                     if (wordReminder.equals(suffix.base())) {
                         return new String[] { word.code(), suffix.code() };
                     }
@@ -275,6 +382,8 @@ public class Engine {
             }
         }
 
+        // if we get here - nothing was found
+        // return code zero - [???]
         return new String[] { "0" };
     }
 
--- ie/atu/sw/FileIO.java
+++ ie/atu/sw/FileIO.java
@@ -1,11 +1,13 @@
 package ie.atu.sw;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class FileIO {
+    // Method read text file and return result as array of String
     public static String[] readFile(String fileName) throws Exception {
         try {
             return Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8).toArray(new String[0]);
@@ -14,6 +16,7 @@ public class FileIO {
         }
     }
 
+    // Method recieves fileName and String array and write it to the file
     public static void writeFile(String fileName, String[] lines) throws Exception {
         try (var fileWriter = new FileWriter(fileName)) {
             for (var i = 0; i < lines.length; i++) {
@@ -21,6 +24,22 @@ public class FileIO {
             }
         } catch (Exception e) {
             throw new Exception("Error writing file: " + fileName);
+        }
+    }
+
+    // Method validates entered file name
+    public static boolean validateStringFilenameUsingIO(String filename) {
+        File file = new File(filename);
+        boolean created = false;
+        try {
+            created = file.createNewFile();
+            return created;
+        } catch (Exception e) {
+            return created;
+        } finally {
+            if (created) {
+                file.delete();
+            }
         }
     }
 }
